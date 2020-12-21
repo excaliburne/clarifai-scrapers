d750443977cfdda2ac4039c13f5495de5d00b091
excaliburne <jeremy.faret@clarifai.com>
2020-12-21 19:02:45 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='clarifai_scrapers',
+    version="0.0.1",
+    install_requires=[],
+    description="Clarifai Scrapers",
+    author='Jeremy',
+    author_email='',
+    url='https://github.com/theatlantic/django-nested-admin'
+)
